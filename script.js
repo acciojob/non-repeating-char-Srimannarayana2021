e76a32c723e285f7562d19bc8943526f4a7d5ea8
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-08-29 21:38:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	for(int i=1;i<str.size()-1;i++){
-		if(str[i-1] != str[i] && str[i] != str[i+1]) return str[i];
+		if(str[i-1] !== str[i] && str[i] !== str[i+1]) return str[i];
 	}
 	return null;
 }
